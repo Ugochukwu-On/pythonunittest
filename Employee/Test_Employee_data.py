e8c3b84e95f506b845fcde543dba54b0484b05f7
Ugochukwu-On <onwuu1@gmail.com>
2021-07-18 17:03:26 -0700

    Made a directory for Employee_data and its unittest

---
+++ Employee/Test_Employee_data.py
@@ -0,0 +1,17 @@
+import unittest
+from Test.Employee import Employee_data
+
+
+class TestEmployeeData(unittest.TestCase):
+
+    def test_Employee_Data(self):
+        pass
+
+    def test_email(self):
+        x = Employee_data.Employee('uncle', 'ben')
+        #email = 'uncle' + 'ben' + '@gmail.com'
+        self.assertEqual(x.email, 'uncleben@gmail.com')
+
+
+if __name__ == '__main__':
+    unittest.main()
