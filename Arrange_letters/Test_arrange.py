7b260b2183938ed0cf069845d61f26cf9e1f60b2
Ugochukwu-On <onwuu1@gmail.com>
2021-07-18 17:01:27 -0700

    Made a directory for arrange and its unittest

--- Arrange_letters/Test_arrange.py
+++ Arrange_letters/Test_arrange.py
@@ -1,5 +1,5 @@
 import unittest
-import arrange
+from Test.Arrange_letters import arrange
 
 
 class TestArrange(unittest.TestCase):
@@ -9,13 +9,13 @@ class TestArrange(unittest.TestCase):
 
     def test_words(self):
         self.assertEqual(arrange.words(["g", "e", "o"], [0, 1, 2]), 'geo')
-        self.assertEqual(arrange.words(["e","l", "l", "o", "h"], [4, 0, 1, 2, 3]), 'hello')
+        self.assertEqual(arrange.words(["e", "l", "l", "o", "h"], [4, 0, 1, 2, 3]), 'hello')
 
     def test_words_reject_argument(self):
         self.assertNotEqual(arrange.words(["g", "e", "o"], [0, 1, 2]), 'ge o')
         self.assertNotEqual(arrange.words(["e", "l", "l", "o", "h"], [4, 0, 1, 2, 3]), 'he llo')
 
     def test_words_god(self):
-        self.assertNotIn(arrange.words(["g", "e", "o"],[0, 1, 2]), 'hello')
+        self.assertNotIn(arrange.words(["g", "e", "o"], [0, 1, 2]), 'hello')
         self.assertNotIn(arrange.words(["e", "l", "l", "o", "h"], [4, 0, 1, 2, 3]), 'geo')
 
